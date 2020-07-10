d2747b46e99684052a11292a71b69cee63093525
Bogdan-Gabriel Roman <bogdan-gabriel.roman@nxp.com>
2020-07-10 14:24:03 +0300

    Default OP-TEE configurations for S32G274A platforms
    Issue: ALB-6128
    Signed-off-by: Bogdan-Gabriel Roman <bogdan-gabriel.roman@nxp.com>

---
+++ core/arch/arm/plat-s32g/main.c
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright 2020 NXP
+ */
+
+#include <console.h>
+#include <drivers/gic.h>
+#include <drivers/s32g_uart.h>
+#include <kernel/boot.h>
+#include <kernel/panic.h>
+#include <mm/core_mmu.h>
+#include <platform_config.h>
+
+static struct gic_data gic_data;
+static struct s32g_uart_data console_data;
+
+register_phys_mem(MEM_AREA_IO_NSEC, CONSOLE_UART_BASE,
+		  CONSOLE_UART_SIZE);
+
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, GICD_BASE, CORE_MMU_PGDIR_SIZE);
+
+void itr_core_handler(void)
+{
+	gic_it_handle(&gic_data);
+}
+
+void main_init_gic(void)
+{
+	vaddr_t gicd_base;
+
+	gicd_base = core_mmu_get_va(GICD_BASE, MEM_AREA_IO_SEC);
+	if (!gicd_base)
+		panic();
+
+	/* Initialize GIC */
+	gic_init_base_addr(&gic_data, 0, gicd_base);
+	itr_init(&gic_data.chip);
+}
+
+void console_init(void)
+{
+	s32g_uart_init(&console_data, CONSOLE_UART_BASE);
+	register_serial_console(&console_data.chip);
+}
