2c1f6b3fd501b44145a7944a40772001086f9caa
Bogdan-Gabriel Roman <bogdan-gabriel.roman@nxp.com>
2021-11-10 01:34:52 +0200

    s32g: add s32g3 platform support
    The s32g3 plaform has more cores than its s32g2 sibling and a different
    GICR address. This commit updates the corresponding build flags and
    definitions accordingly.
    
    The s32g3 plaform will be built with the same PLATFORM=s32g flag.
    The distinction between the two platforms from the same family
    will be done using the PLATFORM_FLAVOR flag. Hence, s32g2 builds will
    use PLATFORM_FLAVOR=s32g2 and s32g3 build will use PLATFORM_FLAVOR=s32g3.
    
    Issue: ALB-8088
    Signed-off-by: Bogdan-Gabriel Roman <bogdan-gabriel.roman@nxp.com>

--- core/arch/arm/plat-s32g/platform_config.h
+++ core/arch/arm/plat-s32g/platform_config.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright 2020 NXP
+ * Copyright 2020-2021 NXP
  */
 
 #ifndef PLATFORM_CONFIG_H
@@ -11,9 +11,14 @@
 #define CONSOLE_UART_BASE		(0x401C8000)
 #define CONSOLE_UART_SIZE       (0x4000)
 
+#if defined(PLATFORM_FLAVOR_s32g2)
+#define GICR_OFFSET (0x80000)
+#elif defined(PLATFORM_FLAVOR_s32g3)
+#define GICR_OFFSET (0x100000)
+#endif
+
 #define GIC_BASE    (0x50800000)
 #define GICD_OFFSET (0x0)
-#define GICR_OFFSET (0x80000)
 #define GICD_BASE   (GIC_BASE + GICD_OFFSET)
 #define GICR_BASE   (GIC_BASE + GICR_OFFSET)
 
