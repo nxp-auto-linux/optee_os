6cd0914f2201fb7eda0edd831eba08a53fc0030d
Bogdan Roman <bogdan-gabriel.roman@nxp.com>
2022-04-14 17:14:50 +0300

    plat-s32: enable core dynamic shared memory
    Enable the default selection of core dynamic shared memory.
    Set CFG_CORE_ARM64_PA_BITS to 40, otherwise optee core would not know
    how to acces the second bank of DRAM whose address is beyond 32 bits.
    Even though optee_os does not currently use the second bank of DRAM,
    it would error out during parsing of the non-secure DTB at boot time
    when detecting the second bank of memory.
    
    Issue: ALB-8800, ALB-9300
    Signed-off-by: Bogdan Roman <bogdan-gabriel.roman@nxp.com>

--- core/arch/arm/plat-s32/conf.mk
+++ core/arch/arm/plat-s32/conf.mk
@@ -8,6 +8,7 @@ include core/arch/arm/cpu/cortex-armv8-0.mk
 
 $(call force,CFG_ARM64_core,y)
 $(call force,CFG_CORE_LARGE_PHYS_ADDR,y)
+$(call force,CFG_CORE_ARM64_PA_BITS,40)
 $(call force,CFG_WITH_ARM_TRUSTED_FW,y)
 
 # Enabling the Pager requires access to SRAM
@@ -62,4 +63,3 @@ CFG_TZDRAM_SIZE ?= 	0x01400000 # 20MB
 CFG_SHMEM_START ?=	($(CFG_TZDRAM_START) + $(CFG_TZDRAM_SIZE))
 CFG_SHMEM_SIZE ?= 	0x00200000 # 2MB
 CFG_CORE_RESERVED_SHM ?= y
-CFG_CORE_DYN_SHM ?= n
