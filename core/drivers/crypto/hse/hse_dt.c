47334a0c5a426c6a01ef997bcc394dc2fca57d64
Bogdan Roman <bogdan-gabriel.roman@nxp.com>
2022-10-27 11:31:12 +0300

    drivers: hse: get MU RX interrupt from the DT
    Add support for retrieving HSE MU's RX interrupt number from the device-
    tree.
    
    Issue: ALB-8989
    Signed-off-by: Bogdan Roman <bogdan-gabriel.roman@nxp.com>

--- core/drivers/crypto/hse/hse_dt.c
+++ core/drivers/crypto/hse/hse_dt.c
@@ -9,6 +9,8 @@
 #include <libfdt.h>
 #include <trace.h>
 
+#define GIC_MAX_IRQS	1020
+
 static const char *hse_compatible = "nxp,s32cc-hse";
 
 static int fdt_find_compatible_enabled(void *fdt, const char *compatible)
@@ -85,6 +87,81 @@ static int fdt_get_reg_props_by_index(const void *dtb, int node, int index,
 	return 0;
 }
 
+static int fdt_read_irq_cells(const fdt32_t *prop, int nr_cells)
+{
+	int it_num;
+	uint32_t res;
+
+	if (!prop || nr_cells < 2)
+		return DT_INFO_INVALID_INTERRUPT;
+
+	res = fdt32_to_cpu(prop[1]);
+	if (res >= GIC_MAX_IRQS)
+		return DT_INFO_INVALID_INTERRUPT;
+
+	it_num = (int)res;
+
+	switch (fdt32_to_cpu(prop[0])) {
+	case 1:
+		it_num += 16;
+		break;
+	case 0:
+		it_num += 32;
+		break;
+	default:
+		it_num = DT_INFO_INVALID_INTERRUPT;
+	}
+
+	return it_num;
+}
+
+static int fdt_get_irq_props_by_index(const void *dtb, int node,
+				      unsigned int index, int *irq_num)
+{
+	const fdt32_t *prop;
+	int parent, len = 0;
+	uint32_t ic, cell, res;
+
+	parent = fdt_parent_offset(dtb, node);
+	if (parent < 0)
+		return -FDT_ERR_BADOFFSET;
+
+	prop = fdt_getprop(dtb, parent, "#interrupt-cells", NULL);
+	if (!prop) {
+		IMSG("Couldn't find \"#interrupts-cells\" property in dtb\n");
+		return -FDT_ERR_NOTFOUND;
+	}
+
+	ic = fdt32_to_cpu(*prop);
+
+	if (MUL_OVERFLOW(index, ic, &cell))
+		return -FDT_ERR_BADVALUE;
+
+	prop = fdt_getprop(dtb, node, "interrupts", &len);
+	if (!prop) {
+		IMSG("Couldn't find \"interrupts\" property in dtb\n");
+		return -FDT_ERR_NOTFOUND;
+	}
+
+	if (ADD_OVERFLOW(cell, ic, &res))
+		return -FDT_ERR_BADVALUE;
+
+	if (MUL_OVERFLOW(res, sizeof(uint32_t), &res))
+		return -FDT_ERR_BADVALUE;
+
+	/* res = (cell + ic) * sizeof(uint32_t) */
+	if (res > (unsigned int)len)
+		return -FDT_ERR_BADVALUE;
+
+	if (irq_num) {
+		*irq_num = fdt_read_irq_cells(&prop[cell], ic);
+		if (*irq_num < 0)
+			return -FDT_ERR_BADVALUE;
+	}
+
+	return 0;
+}
+
 int hse_dt_get_regs(paddr_t *regs_base, size_t *regs_size,
 		    paddr_t *desc_base, size_t *desc_size)
 {
@@ -129,6 +206,37 @@ int hse_dt_get_regs(paddr_t *regs_base, size_t *regs_size,
 		return ret;
 	*desc_base = base;
 	*desc_size = size;
+
+	return 0;
+}
+
+int hse_dt_get_irq(int *rx_irq)
+{
+	void *fdt = NULL;
+	int offset;
+	int rx_irq_off;
+	int ret;
+
+	fdt = get_dt();
+	if (!fdt) {
+		EMSG("No Device Tree found");
+		return -1;
+	}
+
+	offset = fdt_find_compatible_enabled(fdt, hse_compatible);
+	if (offset < 0) {
+		EMSG("Could not find node with matching compatible \"%s\"",
+		     hse_compatible);
+		return offset;
+	}
+	rx_irq_off = fdt_stringlist_search(fdt, offset, "interrupt-names",
+					   "hse-rx");
+	if (rx_irq_off < 0)
+		return rx_irq_off;
+
+	ret = fdt_get_irq_props_by_index(fdt, offset, rx_irq_off, rx_irq);
+	if (ret < 0)
+		return ret;
 
 	return 0;
 }
--- core/drivers/crypto/hse/include/hse_dt.h
+++ core/drivers/crypto/hse/include/hse_dt.h
@@ -10,4 +10,5 @@
 
 int hse_dt_get_regs(paddr_t *regs_base, size_t *regs_size,
 		    paddr_t *desc_base, size_t *desc_size);
+int hse_dt_get_irq(int *rx_irq);
 #endif /* HSE_DT_H */
